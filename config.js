d05a55271c03fd706ec53d6d3eb625305417960a
Saujiro <110501201+Saujiro@users.noreply.github.com>
2023-03-19 13:23:57 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 module.exports = {
-    token: 'XXXXXXXXXXXXXXXXXX', //Token du bot
+    token: 'MTAwNTI3OTA2NDEzNjg4NDI3NA.GufT5Y.DMefpIXRG1eNElwpy-rk1XF5vMB3i-nXs6gjlk', //Token du bot
     botId: 'XXXXXXXXXXXXXXXXXX', //Id du bot
     owner: 'XXXXXXXXXXXXXXXXXX', //Id du Propriétaire
     prefix: '!',//Prefix du bot
     embed: {
         color: 'AQUA', //Embed Color
-        footer: 'Template by Derp#5777' //Embed Footer
+        footer: 'Template by Sôjiro#5077' //Embed Footer
     },
     plugin: {
-        server: 'XXXXXXXXXXXXXXXXXX',//Id du serveur
+        server: '1086983730956079224',//Id du serveur
         welcome: {
             enabled: true,// true = plugin activé || false = plugin desactivé
             channel: 'XXXXXXXXXXXXXXXXXX',//id du channel de bienvenue
